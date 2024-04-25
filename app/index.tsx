3a0020c61ef45497881b63542ec6cb169f7fea87
Adil Muhammad <ivgcomunity@gmail.com>
2024-04-25 13:12:22 +0700

    initialize by @ini_adil

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+type indexProps = {};
+
+const index: React.FC<indexProps> = () => {
+  return (
+    <>
+      <View className="flex flex-1 justify-center items-center">
+        <Text>Template by @ini_adil</Text>
+      </View>
+    </>
+  );
+};
+export default index;
